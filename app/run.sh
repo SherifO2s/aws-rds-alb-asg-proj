db7ac079b676b0f536d6d88fd87e6a38e89fec3f
Sherif Otufowora <sherif@llamalab.ai>
2026-05-13 14:22:45 +0100

    Refactor run and user-data scripts for improved deployment. Removed daemon mode from gunicorn command in run.sh for better process management. Updated user-data.sh to streamline application setup, including virtual environment creation and direct execution of run.sh with necessary environment variables.

--- app/run.sh
+++ app/run.sh
@@ -3,7 +3,7 @@
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-gunicorn app:app --bind 0.0.0.0:8000 --daemon
+gunicorn app:app --bind 0.0.0.0:8000 
 
 
 
--- app/user-data.sh
+++ app/user-data.sh
@@ -4,8 +4,20 @@ sudo yum install git -y
 
 sudo git clone https://github.com/SherifO2s/aws-rds-alb-asg-proj.git
 
-export DB_LINK="postgresql://postgres:Admin1234@april-asg-app.c1cmay622txx.eu-west-2.rds.amazonaws.com:5432/mydb"
+cd aws-rds-alb-asg-proj/app
 
-sudo chmod u+x aws-rds-alb-asg-proj/app/run.sh
+sudo chmod u+x run.sh
 
-bash aws-rds-alb-asg-proj/app/run.sh &
+# in path day2/app
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+export DB_LINK=postgresql://postgres:Admin1234@april-asg-app.c1cmay622txx.eu-west-2.rds.amazonaws.com:5432/mydb
+
+gunicorn run:app --bind 0.0.0.0:8000 &
+
+
+# psql -h april-asg-app.c1cmay622txx.eu-west-2.rds.amazonaws.com -d mydb -U postgres
+
+# pg_restore -d $DB_LINK -F c -j 4 -v mydb_20260503_004353.dump
